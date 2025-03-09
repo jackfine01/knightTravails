ddd60e2f9241574e5c4bf5fc19fb5a05c5eb0c77
jack <jackfine01@gmail.com>
2025-03-09 14:28:18 -0400

    starting with knightTravails function, conditional checks, and a Node class.

--- main.js
+++ main.js
@@ -19,26 +19,31 @@ class Node{
     }
 };                    
 
-function inBounds(position){
-    let x = position[0]
-    let y = position[1]
-    let yHigh   = (y > 7);
-    let yLow    = (y < 0);
-    let xHigh   = (x > 7);
-    let xLow    = (x < 0);
-
-    if(!yHigh&&!yLow&&!xHigh&&!xLow){
-        return true;
-    } else
-        return false;
-}
-
-function notVisited(node, visited){
-    console.log(node)
-    for(let i = 0; i < visited.length; i++){
-        if(visited[i] === node.position)
+function knightTravails(start, end){
+    function inBounds(position){
+        let x = position[0]
+        let y = position[1]
+        let yHigh   = (y > 7);
+        let yLow    = (y < 0);
+        let xHigh   = (x > 7);
+        let xLow    = (x < 0);
+    
+        if(!yHigh&&!yLow&&!xHigh&&!xLow){
+            return true;
+        } else
             return false;
     }
-    return true;
+    
+    function notVisited(node, visited){
+        console.log(node)
+        for(let i = 0; i < visited.length; i++){
+            if(visited[i] === node)
+                return false;
+        }
+        return true;
+    }
+    const startNode = new Node(start)
+    const endNode = new Node(end)
+    const visitedNodes = [startNode];
 }
-let visited = [];
+
